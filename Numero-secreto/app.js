23b02ae0a33b698373d70e5aa87187b5bcd4a7f7
RobsonCoura <robson_coura@yahoo.com.br>
2024-07-22 11:51:55 -0300

    Retirando os alert e manipulando textos no HTML

--- Numero-secreto/app.js
+++ Numero-secreto/app.js
@@ -1,32 +1,9 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+// Obtém a referência do primeiro elemento <h1> encontrado no documento
+let titulo = document.querySelector('h1');
+// Isso altera o título da página para "Jogo do número secreto"
+titulo.innerHTML = 'Jogo do número secreto';
 
-// enquanto chute não for igual ao n.s.
-while (chute != numeroSecreto) {
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        // tentativas = tentativas + 1;
-        tentativas++;
-    }
-}
-
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-// if (tentativas > 1) {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-// }
+// Obtém a referência do primeiro elemento <p> encontrado no documento
+let paragrafo = document.querySelector('p');
+// Isso define a descrição ou instrução para o usuário, indicando a faixa de números válidos
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
