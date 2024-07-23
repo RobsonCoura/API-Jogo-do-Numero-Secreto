3edbc4b50804519108e93ec3806dfda4c6049777
RobsonCoura <robson_coura@yahoo.com.br>
2024-07-22 21:39:51 -0300

    Adicionando JavaScript para manipular o HTML

--- Numero-secreto/app.js
+++ Numero-secreto/app.js
@@ -1,9 +1,13 @@
-// Obtém a referência do primeiro elemento <h1> encontrado no documento
-let titulo = document.querySelector('h1');
-// Isso altera o título da página para "Jogo do número secreto"
-titulo.innerHTML = 'Jogo do número secreto';
+let titulo = document.querySelector('h1'); // Obtém a referência do primeiro elemento <h1> encontrado no documento
 
-// Obtém a referência do primeiro elemento <p> encontrado no documento
-let paragrafo = document.querySelector('p');
-// Isso define a descrição ou instrução para o usuário, indicando a faixa de números válidos
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+titulo.innerHTML = 'Jogo do número secreto'; // Isso altera o título da página para "Jogo do número secreto"
+
+
+let paragrafo = document.querySelector('p'); // Obtém a referência do primeiro elemento <p> encontrado no documento
+
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; // Isso define a descrição ou instrução para o usuário, indicando a faixa de números válidos
+
+
+function verificarChute() {
+    console.log('O botao foi clicado')
+}
